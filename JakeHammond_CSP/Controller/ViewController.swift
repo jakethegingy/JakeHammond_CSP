ed573272f5107f9193e250a3b04a3f266bef6bd2
jakethegingy <31513857+jakethegingy@users.noreply.github.com>
2017-11-08 08:41:51 -0700

    Initilized everything. Created the display as well and linked in the button.

---
+++ JakeHammond_CSP/Controller/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  JakeHammond_CSP
+//
+//  Created by Hammond, Jacob on 11/8/17.
+//  Copyright © 2017 Hammond, Jacob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    // MARK: Data members
+    @IBOutlet weak var algorithmText: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
