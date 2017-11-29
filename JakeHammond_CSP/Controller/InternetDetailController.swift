25912b2f9979a84789eac8df1419315e534a38ac
jakethegingy <31513857+jakethegingy@users.noreply.github.com>
2017-11-29 08:51:58 -0700

    more mucho stuff

---
+++ JakeHammond_CSP/Controller/InternetDetailController.swift
@@ -0,0 +1,15 @@
+//
+//  InternetDetailController.swift
+//  JakeHammond_CSP
+//
+//  Created by Hammond, Jacob on 11/29/17.
+//  Copyright © 2017 Hammond, Jacob. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class InternetDetailViewController : UIViewController
+{
+    //MARK: GUI Controls
+}
---
+++ JakeHammond_CSP/Controller/InternetMasterController.swift
@@ -0,0 +1,9 @@
+//
+//  InternetMasterController.swift
+//  JakeHammond_CSP
+//
+//  Created by Hammond, Jacob on 11/29/17.
+//  Copyright © 2017 Hammond, Jacob. All rights reserved.
+//
+
+import Foundation
