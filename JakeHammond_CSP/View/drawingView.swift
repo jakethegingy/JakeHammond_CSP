8b9d672b13ed3f07f7596135855db17f680bba6f
jakethegingy <31513857+jakethegingy@users.noreply.github.com>
2017-12-05 09:52:41 -0700

    I fixed all of my errors, there were like a thousand.

--- JakeHammond_CSP/View/drawingView.swift
+++ JakeHammond_CSP/View/drawingView.swift
@@ -42,4 +42,34 @@ public class DrawingView: UIView
         
         return stickFigure
     }
+    
+    public func drawHappyTree() -> Void
+    {
+        let bobRoss = UIBezierPath()
+        
+        bobRoss.move(to: CGPoint(x:80, y:50))
+        bobRoss.addLine(to: CGPoint(x: 120, y: 150))
+        bobRoss.addLine(to: CGPoint(x: 40, y: 150))
+        bobRoss.close()
+        UIColor(patternImage: UIImage(named: "DerpyLlama")!).setFill()
+        UIColor.brown.setStroke()
+        bobRoss.lineWidth = 2.0
+        bobRoss.fill()
+        bobRoss.stroke()
+        
+        let happyTree = UIBezierPath()
+        
+        UIColor.green.setFill()
+        happyTree.move(to: CGPoint(x: 110, y: 150))
+        happyTree.addLine(to: CGPoint(x: 150, y: 200))
+        happyTree.addLine(to: CGPoint(x: 10, y: 150))
+        happyTree.addLine(to: CGPoint(x: 50, y: 150))
+        happyTree.close()
+        happyTree.stroke()
+        happyTree.fill()
+        happyTree.move(to: CGPoint(x: 80, y: 200))
+        happyTree.addLine(to: CGPoint(x: 80, y: 250))
+        happyTree.lineWidth = 6.0
+        happyTree.stroke()
+    }
 }
